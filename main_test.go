385014cb8f94d3e4ec30771d90055c299216f990
Chavez Harris <74829200+softiosolutions@users.noreply.github.com>
2022-03-04 15:42:27 -0400

    removed duplicated code

--- main_test.go
+++ main_test.go
@@ -15,11 +15,14 @@ func TestAPIEndpoints(t *testing.T) {
 	// data of a single test case. This structure is then used
 	// to create a so called test map, which contains all test
 	// cases, that should be run for testing this function
-	GETRequestMethodTests := []struct {
+	tests := []struct {
 		description string
 
 		// Test input
 		route string
+
+		// Method
+		method string
 
 		// Expected output
 		expectedError bool
@@ -33,6 +36,7 @@ func TestAPIEndpoints(t *testing.T) {
 		{
 			description:   "index route",
 			route:         "/api/v1/status",
+			method: 		"GET",
 			expectedError: false,
 			expectedCode:  200,
 			expectedBody:  "OK",
@@ -40,9 +44,18 @@ func TestAPIEndpoints(t *testing.T) {
 		{
 			description:   "non existing route",
 			route:         "/api/v1/status/i-dont-exist",
+			method: 		"GET",
 			expectedError: false,
 			expectedCode:  404,
 			expectedBody:  "Cannot GET /api/v1/status/i-dont-exist",
+		},
+		{
+			description: "index route",
+			route:        "/api/v1/hello",
+			method: 	  "POST",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  "Hello, John Doe!",
 		},
 
 	}
@@ -50,17 +63,46 @@ func TestAPIEndpoints(t *testing.T) {
 	// Setup the app as it is done in the main function
 	app := Setup()
 
+	// Global Request Var
+	var req *http.Request;
+
+	type Body struct {
+		Name string `json:"name"`
+	}
+
+	body := Body{
+		Name: "John Doe",
+	}
+
+	personJson, _ := json.Marshal(body)
+
 
 
 	// Iterate through test single test cases
-	for _, test := range GETRequestMethodTests {
+	for _, test := range tests {
 		// Create a new http request with the route
 		// from the test case
-		req, _ := http.NewRequest(
-			"GET",
-			test.route,
-			nil,
-		)
+
+		if test.method == "POST" {
+
+			req, _ = http.NewRequest(
+				test.method,
+				test.route,
+				bytes.NewBuffer(personJson),
+			)
+
+			req.Header.Set("Content-Type", "application/json")
+
+		} else {
+
+			req, _ = http.NewRequest(
+				test.method,
+				test.route,
+				nil,
+			)
+
+		}
+
 
 		// Perform the request plain with the app.
 		// The -1 disables request latency.
@@ -88,82 +130,6 @@ func TestAPIEndpoints(t *testing.T) {
 		// Verify, that the reponse body equals the expected body
 		assert.Equalf(t, test.expectedBody, string(body), test.description)
 	}
-
-
-
-	// Test POST Request Methods
-	POSTRequestMethodTests := []struct {
-		description string
-
-		// Test input
-		route string
-
-		// Expected output
-		expectedError bool
-		expectedCode  int
-		expectedBody  string
-
-		}{
-			
-		// ReturnHello Endpoint
-		{
-			description: "index route",
-			route:        "/api/v1/hello",
-			expectedError: false,
-			expectedCode:  200,
-			expectedBody:  "Hello, John Doe!",
-		},
-
-	}
-
-	type Person struct {
-		Name string `json:"name"`
-	}
-
-	person := Person{
-		Name: "John Doe",
-	}
-
-	personJson, _ := json.Marshal(person)
-
-
-	for _, test := range POSTRequestMethodTests {
-
-	req, _ := http.NewRequest(
-		"POST",
-		test.route,
-		bytes.NewBuffer(personJson),
-	)
-
-	req.Header.Set("Content-Type", "application/json")
-
-	// Perform the request plain with the app.
-	// The -1 disables request latency.
-	res, err := app.Test(req, -1)
-
-	// verify that no error occured, that is not expected
-	assert.Equalf(t, test.expectedError, err != nil, test.description)
-
-	// test case needs to be processed
-	if test.expectedError {
-		continue
-	}
-
-
-	// Verify if the status code is as expected
-	assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
-
-	// Read the response body
-	body, err := ioutil.ReadAll(res.Body)
-
-	// Reading the response body should work everytime, such that
-	// the err variable should be nil
-	assert.Nilf(t, err, test.description)
-
-	// Verify, that the reponse body equals the expected body
-	assert.Equalf(t, test.expectedBody, string(body), test.description)
-
-}
 
 }
 
